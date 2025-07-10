2d326c325f04033d320a350ebac3df029a03e5a9
tojakar <167592470+tojakar@users.noreply.github.com>
2025-07-10 16:15:16 -0400

    Revert "made gitiginore"
    This reverts commit c96a40eb6e53c3aab0d4e7a19d0526b6d30b2d86.

---
+++ cards/frontend/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+
+function NavBar() {
+
+
+  
+  return (
+    <div style={{ 
+        position: 'absolute', 
+        top: 65, 
+        left: 570,
+        }}>
+        <span id="Nav">Home </span>
+        <span id="Nav">Map </span>
+        <span id="Nav"> About </span><br />
+
+    </div>
+  );
+}
+
+export default NavBar;
