c96a40eb6e53c3aab0d4e7a19d0526b6d30b2d86
tojakar <167592470+tojakar@users.noreply.github.com>
2025-07-10 16:11:47 -0400

    made gitiginore

--- README.md
+++
@@ -1,1 +0,0 @@
-This repository holds our group's large project for COP4331. This project includes a website and mobile version using a MERN stack.
--- cards/frontend/src/components/Path.ts
+++
@@ -1,9 +0,0 @@
-const app_name = 'group26.xyz';
-
-export function buildPath(route: string): string {
-  if (process.env.NODE_ENV !== 'development') {
-    return 'http://' + app_name + ':5001/' + route;
-  } else {
-    return 'http://localhost:5001/' + route;
-  }
-}
