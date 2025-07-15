36a478d2c64f5ebe5b3919d313bdfb166fed92ba
samiam1113 <samiam2539@gmail.com>
2025-07-15 12:55:07 -0400

    took off nav bar for register user

--- cards/frontend/src/pages/RegisterPage.tsx
+++ cards/frontend/src/pages/RegisterPage.tsx
@@ -1,13 +1,12 @@
 import PageTitle from '../components/PageTitle.tsx';
 import RegisterUser from '../components/RegisterUser.tsx';
-import NavBar from '../components/NavBar.tsx';
+
 const RegisterPage = () =>
 {
     return(
     <div>
     <PageTitle />
     <RegisterUser />
-    <NavBar/>
     </div>
     );
 };
