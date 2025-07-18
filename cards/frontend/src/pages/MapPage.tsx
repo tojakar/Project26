e72f8bd6644745aa7b02bc8dece0226aba0ab9cf
scoutrogers <rogers.s0701@gmail.com>
2025-07-17 20:53:37 -0400

    Update MapPage.tsx
    import StarRating to MapPage

--- cards/frontend/src/pages/MapPage.tsx
+++ cards/frontend/src/pages/MapPage.tsx
@@ -1,6 +1,7 @@
 import Map from '../components/Map.tsx';
 import NavBar from '../components/NavBar.tsx';
 import PageTitle from '../components/PageTitle.tsx'
+import StarRating from '../components/StarRating';
 
 
 const MapPage = () =>
