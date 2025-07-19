f36b1a443f6e103f1b12691f3ba4e62479fa3ba3
DigiR4Z0R <luisdavidde04@gmail.com>
2025-07-19 19:56:49 -0400

    Some more bugs that didnt go with the previous commit.

--- TestingFlutter/flutter_application_1/test/widget_test.dart
+++ TestingFlutter/flutter_application_1/test/widget_test.dart
@@ -1,30 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_application_1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Landing screen shows app title and buttons', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Check that the app title appears
+    expect(find.text('Water Watch'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Check for the subtitle
+    expect(find.text('Find your favorite fountain'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Check for Register and Login buttons
+    expect(find.text('Register'), findsOneWidget);
+    expect(find.text('Login'), findsOneWidget);
   });
 }
